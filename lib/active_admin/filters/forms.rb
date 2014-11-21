a1609d2c2ef78f992e0e0cec8f7d00854f1a6d08
Zhomart Mukhamejanov <mzhomart@gmail.com>
2014-11-20 19:31:57 -0800

    filters/forms changed to handle mongoid

--- lib/active_admin/filters/forms.rb
+++ lib/active_admin/filters/forms.rb
@@ -12,9 +12,22 @@ module ActiveAdmin
     class FormBuilder < ::ActiveAdmin::FormBuilder
       include ::ActiveAdmin::Filters::FormtasticAddons
 
-      def filter(method, options = {})
-        if method.present? && options[:as] ||= default_input_type(method)
-          template.concat input(method, options)
+      if defined?(ActiveRecord)
+        def filter(method, options = {})
+          if method.present? && options[:as] ||= default_input_type(method)
+            template.concat input(method, options)
+          end
+        end
+      elsif defined?(Mongoid)
+        def filter(method, options = {})
+          if method.present? && options[:as] ||= default_input_type(method)
+            if reflection_for(method)
+              # ransack will add suffix '_id'
+              template.concat input(method.to_s.chomp('_id'), options)
+            else
+              template.concat input(method, options)
+            end
+          end
         end
       end
 
@@ -22,9 +35,32 @@ module ActiveAdmin
 
       protected
 
+      def default_mongoid_input_type(method, options = {})
+        if method =~ /_(eq|equals|cont|contains|start|starts_with|end|ends_with)\z/
+          :string
+        elsif klass._ransackers.key?(method.to_s)
+          klass._ransackers[method.to_s].type
+        elsif reflection_for(method) || polymorphic_foreign_type?(method)
+          :select
+        elsif column = column_for(method)
+          case column.type.to_s
+          when 'Time', 'DateTime'
+            :date_range
+          when 'String', 'BSON::ObjectId'
+            :string
+          when 'Float', 'Integer'
+            :numeric
+          when 'Mongoid::Boolean'
+            :boolean
+          end
+        end
+      end
+
       # Returns the default filter type for a given attribute. If you want
       # to use a custom search method, you have to specify the type yourself.
       def default_input_type(method, options = {})
+        return default_mongoid_input_type(method, options) if defined?(Mongoid)
+
         if method =~ /_(eq|equals|cont|contains|start|starts_with|end|ends_with)\z/
           :string
         elsif klass._ransackers.key?(method.to_s)
