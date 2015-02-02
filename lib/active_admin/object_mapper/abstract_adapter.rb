f84f28fced31cec1f0f93c194003a54555d21f9e
Zhomart Mukhamejanov <mzhomart@gmail.com>
2015-02-01 17:35:50 -0800

    AbstractAdapter: added method delegator

--- lib/active_admin/object_mapper/abstract_adapter.rb
+++ lib/active_admin/object_mapper/abstract_adapter.rb
@@ -6,6 +6,18 @@ module ActiveAdmin
       def initialize(base)
         @base = base
       end
+
+      def method_missing(m, *args, &block)
+        if base.respond_to?(m)
+          base.send(m, *args, &block)
+        else
+          super(m, *args, &block)
+        end
+      end
+
+      def respond_to_missing?(m, include_private)
+        base.respond_to?(m, include_private)
+      end
     end
   end
 end
